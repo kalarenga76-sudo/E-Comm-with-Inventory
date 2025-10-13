1f60a0dd452ccf3b45590f27b66cb65f9c17813c
Kala Renganathan <2024TM93557@wilp.bits-pilani.ac.in>
2025-10-13 09:45:37 +0300

    Initial Commit: Hello Django project

---
+++ hellodjangoapp/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for hellodjangoproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from .views import homePageView
+from .views import numbers_show
+from .views import student_show
+
+urlpatterns = [
+    path('', homePageView, name ='home'),#new load-> http://127.0.0.1:8000/
+    path('list/',numbers_show,name='numbers_show'),
+    path('studentlist/',student_show, name='student_show')
+    
+   
+]
---
+++ hellodjangoapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Student # add this import
+
+def homePageView(request):
+    return HttpResponse("Hello, Django!")
+
+def welcomeView(request):
+    return HttpResponse("My Learning of creating micro services!")
+
+def numbers_show(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1> List of numbers </h1>    The Digits are {0}".format(x))
+
+def student_show(request):
+    students = Student.objects.all()
+    return render(request, "show.html",{'student':students})
+                
